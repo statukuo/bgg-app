0ce36ec8be384cd0b9a063ffd9c0723d4fb3c98c
David Barbero <dbdstk@gmail.com>
2022-04-26 14:22:01 +0200

    Add scheduler to bookings

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'scheduler-react/lib/css/style.css'
 import Context from './Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/bookings/index.js
+++ src/pages/bookings/index.js
@@ -1,46 +1,63 @@
-import { Button, Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import './_style.css';
 import { Component } from 'react';
 import { AppContext } from '../../Context';
-import { map }from 'lodash';
 import CustomNavBar from '../../components/NavBar';
 import { Link } from 'react-router-dom';
+import Paper from '@mui/material/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import { Scheduler, DayView, Appointments, Toolbar, DateNavigator, TodayButton } from '@devexpress/dx-react-scheduler-material-ui';
+import moment from 'moment';
 
 class Bookings extends Component {
   static contextType = AppContext;
   state = {
-    bookingsData: null
+    bookingsData: [],
+    currentDate: new Date()
   }
 
   async componentDidMount() {
-    const bookingsData = await this.context.bookingsService.getAll();
+    const bookingsData = (await this.context.bookingsService.getAll()).map(booking=> {
+      return {
+        title: booking.title,
+        startDate: moment(booking.start_date).format('YYYY-MM-DDTHH:mm:ss'),
+        endDate: moment(booking.end_date).format('YYYY-MM-DDTHH:mm:ss')
+      }
+    });
+    this.tables = await this.context.tablesService.getAll();
+
+    console.log(bookingsData);
 
     this.setState({bookingsData});
   }
 
-  renderBookings(data) {
-    if (!data) {
-      return null;
-    }
-
-    return map(data, (value, id) => {
-      return <Card style={{ width: '18rem' }} key={id}>
-      <Card.Body>
-        <Card.Title>{value.title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{value.author}</Card.Subtitle>
-        <Card.Text>{value.description}</Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-      </Card.Body>
-    </Card>;
-    });
+  currentDateChange(currentDate) {
+    this.setState({ currentDate });
   }
 
   render() {
     return <div className="page">
-      <CustomNavBar/>
+      <CustomNavBar />
       <div className="Bookings">
         <header className="Bookings-header">
-          {this.renderBookings(this.state.bookingsData)}
+          <Paper>
+            <Scheduler
+              data={this.state.bookingsData}
+            >
+              <ViewState
+                currentDate={this.state.currentDate}
+                onCurrentDateChange={(e) => this.currentDateChange(e)}
+              />
+              <DayView
+                startDayHour={0}
+                endDayHour={24}
+              />
+              <Toolbar />
+              <DateNavigator />
+              <TodayButton />
+              <Appointments />
+            </Scheduler>
+          </Paper>
           <Link to="/create-booking">
             <Button variant='success'>Create</Button>
           </Link>
