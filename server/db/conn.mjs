9236ad87ed0b7dd74079232b344079c12a59b543
David Barbero <dbdstk@gmail.com>
2023-05-10 15:08:54 +0200

    Update the linter

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -1,9 +1,11 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const connectionString = process.env.ATLAS_URI || ''
+const connectionString = process.env.ATLAS_URI || "";
 
 try {
-  await mongoose.connect(connectionString)
+    await mongoose.connect(connectionString);
+    mongoose.set("useCreateIndex", true);
 } catch (e) {
-  console.error(e)
+    // eslint-disable-next-line no-console
+    console.error(e);
 }
--- server/server.mjs
+++ server/server.mjs
@@ -1,18 +1,20 @@
-import express from 'express'
-import cors from 'cors'
-import './loadEnvironment.mjs'
-import records from './routes/record.mjs'
-import './db/conn.mjs'
+import express from "express";
+import cors from "cors";
+import "./loadEnvironment.mjs";
+import records from "./routes/record.mjs";
+import "./db/conn.mjs";
 
-const PORT = process.env.PORT || 5050
-const app = express()
+const PORT = process.env.PORT || 5050;
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
 
-app.use('/record', records)
+app.use(express.json());
+
+app.use("/record", records);
 
 // start the Express server
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`)
-})
+    // eslint-disable-next-line no-console
+    console.log(`Server is running on port: ${PORT}`);
+});
