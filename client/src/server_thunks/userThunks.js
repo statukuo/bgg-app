f2bdd82fdf33f854114dc741808c610bf9aec9a9
David Barbero <dbdstk@gmail.com>
2023-05-12 16:12:04 +0200

    Add user not logged in error handling

--- client/src/server_thunks/userThunks.js
+++ client/src/server_thunks/userThunks.js
@@ -1,8 +1,15 @@
 import Axios from "axios";
 import { setUser } from "../slicers/userSlice";
+import { setLoggedin } from "../slicers/authSlice";
 
 export const fetchUserData = () => async (dispatch, getState) => {
-    const response = await Axios.get("http://localhost:5050/user/current", { headers: { Authorization: `Bearer ${getState().auth.token}` } });
+    let response = {};
+    try {
+        response = await Axios.get("http://localhost:5050/user/current", { headers: { Authorization: `Bearer ${getState().auth.token}` } });
+    } catch (error) {
+        dispatch(setLoggedin(false));
+        return;
+    }
 
     dispatch(setUser(response.data));
 };
