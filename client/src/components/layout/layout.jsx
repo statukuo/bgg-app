875f475971a167e5f736c3c5a97a69e1ceae9f6a
Alba <alonso.marmany@gmail.com>
2023-07-20 13:44:58 +0200

    make main min heigh screen

--- client/src/components/layout/layout.jsx
+++ client/src/components/layout/layout.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Header from "./header";
 import BottomNav from "./bottomNav";
-
 const Layout = ({ children }) => {
     return (
         <>
-            <Header />
-            <main className="bg-zinc-700">
-                <div className="grid h-full max-w-lg grid-cols-1 mx-auto gap-3 p-3 ">
-                    {children}
-                </div>
-            </main>
-            <BottomNav />
+            <div className="grid grid-rows-layout min-h-screen">
+                <Header />
+                <main className="bg-zinc-700 flex-grow">
+                    <div className="grid h-full max-w-lg grid-cols-1 mx-auto gap-3 p-3">
+                        {children}
+                    </div>
+                </main>
+                <BottomNav />
+            </div>
         </>
     );
 };
